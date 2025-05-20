b167babc5f548550ec73808537a86cb09e3d9d0d
ParmeetGill422 <parmeetgill422@gmail.com>
2025-05-21 00:47:07 +0530

    1st

--- src/com/example/Person.java
+++ src/com/example/Person.java
@@ -1,4 +1,29 @@
 package com.example;
 
-public class Person {
+public class Person implements Comparable<Person> {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        int lastNameCompare = this.lastName.compareToIgnoreCase(other.lastName);
+        if (lastNameCompare != 0) return lastNameCompare;
+
+        int firstNameCompare = this.firstName.compareToIgnoreCase(other.firstName);
+        if (firstNameCompare != 0) return firstNameCompare;
+
+        return Integer.compare(this.age, other.age); // Bonus: sort by age
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + ", age " + age;
+    }
 }
